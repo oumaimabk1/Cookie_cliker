c9c13a7470d86e56594bb7819a6b3ddb91272bd9
Baccouch oumaima <baccouchoumaima35@gmail.com>
2023-01-11 15:19:52 +0100

    add bonus function

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
 
-const buttonClicker = document.getElementById('clicker')
-const score = document.getElementById('score')
-const btnBonus = document.getElementById('btn-bonus')
-const timer = document.getElementById('timer')
-
+const buttonClicker = document.getElementById("clicker");
+const score = document.getElementById("score");
+const btnBonus = document.getElementById("btn-bonus");
+const timer = document.getElementById("timer");
 
 let count = 0;
-buttonClicker.addEventListener('click',()=>{
-    increment()
+let muliplicateur = 1;
+
+const increment = () => {
+    count += muliplicateur;
+    score.innerText = count;
+};
+
+
+
+buttonClicker.addEventListener("click", () => {
+  increment();
 });
 
-const increment =()=>{
-    count ++;
-    score.innerText=count;
-}
+
+btnBonus.addEventListener("click", () => {
+  let i = 30;
+
+  setInterval(() => {
+    if(i>= 0){
+        timer.innerText = i;
+        muliplicateur = 200;
+    }else{
+        timer.innerText = "Time out!";
+        muliplicateur = 1;
+    }
+   
+    i--;
+  }, 1000);
+
+});
