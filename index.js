53ab16631b39ea4d8b8a8563866ea62f5ed1b03f
ZinebElya <zineb.elyattouti@gmail.com>
2023-01-11 15:59:39 +0100

    auto-click sans timer

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 import * as bootstrap from 'bootstrap'
-
+//Déclarations des variables 
 const buttonClicker = document.getElementById('clicker')
 const score = document.getElementById('score')
 const btnBonus = document.getElementById('btn-bonus')
 const timer = document.getElementById('timer')
-
+const btnAuto = document.getElementById("btn-auto");
 
 let count = 0;
+
+const increment =()=>{
+   count ++;
+   score.innerText=count;
+}
 buttonClicker.addEventListener('click',()=>{
     increment()
 });
 
-const increment =()=>{
-    count ++;
-    score.innerText=count;
-}
+// boutton auto-click
+
+btnAuto.addEventListener("click", () => {
+
+   timer // fonction qui déclenche le timer de 30s
+
+   let clickPerSecond = setInterval(() => {  // A chaque seconde le count s'incrémente de 1 pendant 30s 
+
+      increment()
+   }, 1000);
+
+   if (timer === 0) {                     // apres 30s le compteur arrete de s'incrémenter
+      function finish30s() {
+         clearInterval(clickPerSecond);  
+      }
+   }
+
+ 
+});
+
+
+
