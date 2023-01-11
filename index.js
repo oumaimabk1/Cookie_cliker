4f0039b6a562c0fed9cfe2314f0a2874942cfbb9
Baccouch oumaima <baccouchoumaima35@gmail.com>
2023-01-11 14:32:30 +0100

    add function clicker

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import * as bootstrap from 'bootstrap'
+
+const buttonClicker = document.getElementById('clicker')
+const score = document.getElementById('score')
+
+let count = 0;
+buttonClicker.addEventListener('click',()=>{
+   count ++;
+   score.innerText=count;
+});
+
