9a33dec5987075ab59216a7211c5c2cc7ec40433
ZinebElya <zineb.elyattouti@gmail.com>
2023-01-11 23:52:59 +0100

    corrections autoclick et bonus

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ import * as bootstrap from "bootstrap";
 
 //Déclarations des variables 
 const buttonClicker = document.getElementById('clicker')
-const score = document.getElementById('score')
+const viewScore = document.getElementById("viewScore")
 const btnBonus = document.getElementById('btn-bonus')
-const timer = document.getElementById('timer')
+const timer = document.getElementById('timer')         //le joueur devrait voir une minuterie avec le temps restant à l'intérieur du bouton bonus.
 const btnAuto = document.getElementById("btn-auto");
 const btnReset = document.getElementById("btn-reset" );
 
 
-let count = 0;
+let score = 0;
 let muliplicateur = 1;
 
 const increment = () => {
-    count += muliplicateur;
-    score.innerText = count;
+   score += muliplicateur;
+    viewScore.innerText = score;
 };
 
 
@@ -25,6 +25,7 @@ buttonClicker.addEventListener("click", () => {
   increment();
 });
 
+/*
 //boutton Bonus // test ok 
 
 btnBonus.addEventListener("click", () => {  
@@ -44,8 +45,9 @@ btnBonus.addEventListener("click", () => {
 
 });
 
+*/
 
-
+/*
 // boutton auto-click // test ok 
 // mais à améliorer car il fait rien de spécial 
 // il faut l'incrémenter de 50 ou 100 et faire en sorte qu'il foctionne en meme temps que le clicker !!
@@ -53,7 +55,7 @@ btnBonus.addEventListener("click", () => {
 btnAuto.addEventListener("click", () => {
    let i = 30; 
 
-   let clickPerSecond = setInterval(() => {  // A chaque seconde le count s'incrémente de 1 pendant 30s 
+   let clickPerSecond = setInterval(() => {  // A chaque seconde le score s'incrémente de 1 pendant 30s 
       increment();
       timer.innerText = i;
       i--;
@@ -67,9 +69,37 @@ btnAuto.addEventListener("click", () => {
 });
 
 
+
+*/
+btnAuto.addEventListener("click", () => {
+   setInterval(function(){                   //incrémente le score de 10  toute les 5 secondes
+      score+=10;
+      viewScore.textContent = score;
+  }, 5000);                            
+});
+
+
 // boutton reset // rest ok 
 
 btnReset.addEventListener("click", () =>{
    location.reload();
 });
+
+
+
+
+btnBonus.addEventListener("click", () => {  //le boutton bonus doit accorder un boost de score de 200%
+   let i = 30;
+ 
+   setInterval(() => {
+     if(i>= 0){
+         timer.innerText = i;             //le joueur devrait voir une minuterie avec le temps restant à l'intérieur du bouton bonus.
+         score *= 2;                      //  le score sera multiplié par 2 chaque 30 secondes  
+         viewScore.innerText = score;            
+     }else{
+         timer.innerText = "Time out!"; 
+     }
+     i--;
+   }, 30000);
+ });
 
