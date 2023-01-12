c3d6b4bfcbc3eab3e5ab7c714cb6e591f3d2b95a
Baccouch oumaima <baccouchoumaima35@gmail.com>
2023-01-12 11:09:22 +0100

    modify bonus function

--- index.js
+++ index.js
@@ -1,105 +1,63 @@
-
 import * as bootstrap from "bootstrap";
 
-//Déclarations des variables 
-const buttonClicker = document.getElementById('clicker')
-const viewScore = document.getElementById("viewScore")
-const btnBonus = document.getElementById('btn-bonus')
-const timer = document.getElementById('timer')         //le joueur devrait voir une minuterie avec le temps restant à l'intérieur du bouton bonus.
+//Déclarations des variables
+const buttonClicker = document.getElementById("clicker");
+const viewScore = document.getElementById("viewScore");
+const btnBonus = document.getElementById("btn-bonus");
+const timer = document.getElementById("timer"); //le joueur devrait voir une minuterie avec le temps restant à l'intérieur du bouton bonus.
 const btnAuto = document.getElementById("btn-auto");
-const btnReset = document.getElementById("btn-reset" );
-
+const btnReset = document.getElementById("btn-reset");
 
 let score = 0;
 let muliplicateur = 1;
 
 const increment = () => {
-   score += muliplicateur;
-    viewScore.innerText = score;
+  score += muliplicateur;
+  viewScore.innerText = score;
 };
 
+btnAuto.addEventListener("click", () => {
+  setInterval(function () {
+    //incrémente le score de 10  toute les 5 secondes
+    score += 10;
+    viewScore.textContent = score;
+  }, 5000);
+});
 
+// boutton reset // rest ok
+
+btnReset.addEventListener("click", () => {
+  location.reload();
+});
+
+let startBonus;
+const bonus = btnBonus.addEventListener("click", () => {
+  //le boutton bonus doit accorder un boost de score de 200%
+  startBonus = 5;
+  setInterval(() => {
+    if (startBonus >= 0) {
+      timer.innerText = startBonus;
+    } else {
+      timer.innerText = "Time out!";
+    }
+    startBonus >= 0 ? startBonus-- : startBonus;
+  }, 1000);
+});
+
+clearInterval(bonus,5000)
 //boutton clicker
 
 buttonClicker.addEventListener("click", () => {
-  increment();
-});
-
-/*
-//boutton Bonus // test ok 
-
-btnBonus.addEventListener("click", () => {  
-  let i = 30;
-
-  setInterval(() => {
-    if(i>= 0){
-        timer.innerText = i;
-        muliplicateur = 200;              
-    }else{
-        timer.innerText = "Time out!";
-        muliplicateur = 1;
-    }
-   
-    i--;
-  }, 1000);
-
-});
-
-*/
-
-/*
-// boutton auto-click // test ok 
-// mais à améliorer car il fait rien de spécial 
-// il faut l'incrémenter de 50 ou 100 et faire en sorte qu'il foctionne en meme temps que le clicker !!
-
-btnAuto.addEventListener("click", () => {
-   let i = 30; 
-
-   let clickPerSecond = setInterval(() => {  // A chaque seconde le score s'incrémente de 1 pendant 30s 
+    console.log(startBonus)
+  if (startBonus) {
+    if (startBonus >= 0) {
+      score *= 2;
+      console.log(score); //  le score sera multiplié par 2 chaque 30 secondes
+      viewScore.innerText = score;
+    } else {
       increment();
-      timer.innerText = i;
-      i--;
-   }, 1000);
-
-   setTimeout(() => {                        // apres 30s le compteur arrete de s'incrémenter
-      clearInterval(clickPerSecond);
-      timer.innerText = "Time out!";
-   }, 30000);
-
+    }
+  } else {
+    increment();
+  }
 });
-
-
-
-*/
-btnAuto.addEventListener("click", () => {
-   setInterval(function(){                   //incrémente le score de 10  toute les 5 secondes
-      score+=10;
-      viewScore.textContent = score;
-  }, 5000);                            
-});
-
-
-// boutton reset // rest ok 
-
-btnReset.addEventListener("click", () =>{
-   location.reload();
-});
-
-
-
-
-btnBonus.addEventListener("click", () => {  //le boutton bonus doit accorder un boost de score de 200%
-   let i = 30;
- 
-   setInterval(() => {
-     if(i>= 0){
-         timer.innerText = i;             //le joueur devrait voir une minuterie avec le temps restant à l'intérieur du bouton bonus.
-         score *= 2;                      //  le score sera multiplié par 2 chaque 30 secondes  
-         viewScore.innerText = score;            
-     }else{
-         timer.innerText = "Time out!"; 
-     }
-     i--;
-   }, 30000);
- });
-
