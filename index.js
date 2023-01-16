283d709247d9cd4f3f4fe5c97460b49b653f6c5d
Baccouch oumaima <baccouchoumaima35@gmail.com>
2023-01-16 21:40:22 +0100

    resolve errors

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const displayBanque = async () => {
   for (var i = 0; i < numberOfBuy.length; i++) {
     var item = document.createElement("li");
     item.innerHTML = `<li class="btn m-2 text-info bg-dark">
-    Vous avez acheter le ticket ${numberOfBuy[i].name} ${numberOfBuy[i].numberOfBuy} fois avec ${numberOfBuy[i].Totalcoast} points</li>`;
+    Vous avez acheter le ticket ${numberOfBuy[i].name} ${numberOfBuy[i].numberOfBuy} fois avec ${numberOfBuy[i].TotalCost} points</li>`;
     messages.appendChild(item);
   }
 }
