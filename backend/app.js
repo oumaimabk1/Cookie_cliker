94a72dbf0f612e98e3e77deba0e5ef899a1e37e8
Baccouch oumaima <baccouchoumaima35@gmail.com>
2023-01-17 11:49:05 +0100

    change configuration

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts');
 const cors = require('cors');
+require("dotenv/config")
 
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/auth');
 var playersRouter = require('./routes/Player');
 var multiRouter = require('./routes/multi');
@@ -17,14 +19,10 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //connection to MongoDB.
-mongoose.set("strictQuery", false);
-mongoose.connect('mongodb+srv://user:aXrVVUqnLbKkBcN8@cluster0.qkbvo.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("Connected to Mongo DB"))
-    .catch((err) => console.log("Error", err));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Ejs 
+app.use(expressLayouts);
+app.set('veiw engine', 'ejs')
 
 
 app.use(cors());
@@ -34,8 +32,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', indexRouter);
 app.use('/auth', usersRouter);
 app.use('/players', playersRouter);
 app.use('/multiplicateur', multiRouter);
@@ -43,20 +39,19 @@ app.use('/score', scoreRouter);
 
 app.use(authMiddl);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const port = process.env.PORT || 5000;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const server = http.createServer(app);
+
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+  console.log("Mongodb connected");
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}).catch((err) => {
+  console.log({ err });
+  process.exit(1);
 });
 
 module.exports = app;
